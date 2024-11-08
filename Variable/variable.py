0d98451ce97c049ed68c455ed1939f74e4b1ca09
abulhossainofficial <abulhossain.comc>
2024-11-08 15:57:24 +0600

    variable

---
+++ Variable/variable.py
@@ -0,0 +1,31 @@
+# Python Variables
+
+# Legal variable names:
+myvar = "Hossain"
+my_var = "Hossain"
+_my_var = "Hossain"
+myVar = "Hossain"
+MYVAR = "Hossain"
+myVar4 = "Hossain"
+
+# Illegal variable names:
+1myvar = "Hossain"
+my-var = "Hossain"
+my var = "Hossain"
+my var.1 = "Hossain"
+my_var@ = "Hossain"
+
+# Variable assignment:
+
+myvar = "Hossain"
+my_var = "Hossain"
+myVar = "Hossain"
+myVar4 = "Hossain"
+
+# Variable types:
+
+myvar = "Hossain"       # str
+my_var = 20             # int
+myVar = 20.5            # float
+myVar4 = True           # bool
+myVar5 = [1, 2, 3] 
